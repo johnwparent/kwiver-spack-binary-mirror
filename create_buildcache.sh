88d6411956bbc616c96fd249329d4e46089bb665
John Parent <john.parent@kitware.com>
2026-03-02 17:50:08 -0500

    Add bc creation
    Signed-off-by: John Parent <john.parent@kitware.com>

--- create_buildcache.sh
+++ create_buildcache.sh
@@ -5,6 +5,7 @@ source spack/share/spack/setup-env.sh
 
 spack env activate ./kwiver 
 spack config --scope "env:$(pwd)/kwiver" add "packages:all:prefer:[target=$(spack arch --family --target)]"
+spack config --scope "env:$(pwd)/kwiver" add "config:installer:new"
 spack concretize
 spack -v install -j 10
 spack buildcache push --update-index --with-build-dependencies fletch-buildcache
